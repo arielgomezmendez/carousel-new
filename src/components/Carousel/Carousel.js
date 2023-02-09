dd79ff2fc5e400059f10f9e799c9e76e68beb8ea
Ariel <arielgomezmendez94@gmail.com>
2023-02-08 21:27:44 -0500

    Create transition effect
    Opacity effect, use the default classes created by CSS Transition Library

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,42 +1,43 @@
 import React, { useState } from "react";
 import Card from "../Card/Card";
 import { CSSTransition } from "react-transition-group";
+import { cards } from "../Data/Data";
+import "./Carousel.css";
 
 const Carousel = () => {
   const [index, setIndex] = useState(0);
-  const cards = [
-    {
-      image: "https://picsum.photos/200",
-      description: "Image 1",
-    },
-    {
-      image: "https://picsum.photos/200",
-      description: "Image 2",
-    },
-    {
-      image: "https://picsum.photos/200",
-      description: "Image 3",
-    },
-  ];
+  const [show, setShow] = useState(true);
 
   const handleNext = () => {
-    setIndex(index === cards.length - 1 ? 0 : index + 1);
+    setTimeout(() => {
+      setIndex(index === cards.length - 1 ? 0 : index + 1);
+    }, 300);
+    setShow(false);
+    setTimeout(() => {
+      setShow(true);
+    }, 300);
   };
 
   const handlePrev = () => {
-    setIndex(index === 0 ? cards.length - 1 : index - 1);
+    setTimeout(() => {
+      setIndex(index === 0 ? cards.length - 1 : index - 1);
+    }, 300);
+    setShow(false);
+    setTimeout(() => {
+      setShow(true);
+    }, 300);
   };
 
   return (
-    <div className="carousel-container">
-      <CSSTransition in={true} timeout={500} classNames="card-transition">
+    <section className="carousel-container">
+      <CSSTransition in={show} timeout={3000} classNames="card-transition">
         <Card card={cards[index]} />
       </CSSTransition>
       <div className="buttons-container">
         <button onClick={handlePrev}>Prev</button>
         <button onClick={handleNext}>Next</button>
       </div>
-    </div>
+    </section>
   );
 };
 
---
+++ src/components/Data/Data.js
@@ -0,0 +1,18 @@
+import guitarra from "../assets/guitarra.jpg";
+import asianMale from "../assets/asianMale.jpg";
+import microfono from "../assets/microfono.jpg";
+
+export const cards = [
+  {
+    image: guitarra,
+    description: "Image 1",
+  },
+  {
+    image: asianMale,
+    description: "Image 2",
+  },
+  {
+    image: microfono,
+    description: "Image 3",
+  },
+];
